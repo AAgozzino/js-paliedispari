28356e299180517ca29afcde8c319014a63fa625
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-27 15:02:23 +0200

    Inizio progetto

---
+++ palindroma/js/script.js
@@ -0,0 +1,8 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedo la parola
+
+// reverse parolaUtente
+
+// check parolaUtente == reverse parolaUtente
