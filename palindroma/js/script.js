b80ab36661f5a62ce51b034174065fecae835acb
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-27 15:15:55 +0200

    Esercizio palindromi

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,7 +2,17 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // Chiedo la parola
+var parolaUtente = prompt("Inserisci una parola");
+console.log(parolaUtente);
 
 // reverse parolaUtente
+function reverse(str) {
+  return strReverse = str.split("").reverse().join("");
+};
 
 // check parolaUtente == reverse parolaUtente
+if (parolaUtente == reverse(parolaUtente)) {
+  console.log("palindroma");
+}else {
+  console.log("non è palindroma");
+}
